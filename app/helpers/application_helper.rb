ae21099779b2d9ae89b864e15d7f79e55fa3daa4
Samir Tarverdiyev <samir.tarverdiyev@digit.az>
2021-11-22 18:43:18 +0400

    Add questions inclination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,17 @@ module ApplicationHelper
     if user.avatar_url.present?
       user.avatar_url
     else
-      asset_path 'avatar.jpg'
+      asset_path "avatar.jpg"
     end
+  end
+
+  def inclination(amount, one, few, many)
+    return many if (11..14).include?(amount % 100)
+
+    remain = amount % 10
+
+    return one if remain == 1
+    return few if remain.between?(2,4)
+    many
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,5 +9,22 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const askButton = document.getElementById('ask-button')
+  const askForm = document.getElementById('ask-form')
+
+  askButton.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      setTimeout(() => {
+          const displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+          askForm.style.display = displayStyle
+      }, 300)
+  })
+})
+
+document.getElementById('bodybg').onchange = function () {
+  document.body.style.backgroundColor = this.value;
+}
