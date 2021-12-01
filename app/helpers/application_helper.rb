8679a6fcbd0bcd8704ba65e3361db5933ee580d3
Samir Tarverdiyev <samir.tarverdiyev@digit.az>
2021-12-01 12:26:28 +0400

    Introduce question controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     end
   end
 
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+
   def inclination(amount, one, few, many)
     return many if (11..14).include?(amount % 100)
 
