68ec1cb14156435a4d8f5abe53b076bff8c40917
Samir Tarverdiyev <samir.tarverdiyev@digit.az>
2021-12-01 11:58:48 +0400

    Introduce users and sessions controlers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,60 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
+
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
-  def show
-    @user = User.find params[:id]
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
 
-    @questions = @user.questions
+  def show
+    @questions = @user.questions.order(created_at: :desc)
     @questions_amount = @questions.count
     @answers_amount = @questions.count(&:answer)
     @unanswered_amount = @questions_amount - @answers_amount
 
-    @new_question = Question.new
+    @new_question = @user.questions.build
+  end
+
+  private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users
+
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "show" => "users#show"
+
+  get "sign_up" => "users#new"
+  get "log_out" => "sessions#destroy"
+  get "log_in" => "sessions#new"
+
 end
